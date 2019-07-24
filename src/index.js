d6fec05421d31422a302395247fb113273baca66
Connor McGinnis <connorjaymesmcg@gmail.com>
2019-07-24 10:13:22 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MapSection from "./map";
+import './style.css';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <MapSection> </MapSection>,
   document.getElementById('root')
 )
+
